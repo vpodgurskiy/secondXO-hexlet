80d4738315559a6f71eb3e956b7cfe27ee9b414f
v.podgurskiy <vpodgurskiy@gmail.com>
2018-02-21 22:15:42 +0300

    tests for Field and bug fix in Field

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -2,17 +2,27 @@ package io.hexlet.xo.model;
 
 public class Field {
 
+    private final Figure[][] field;
+
+    Field(int size) {
+        this.field = new Figure[size][size];
+
+    }
+
     int getSize() {
 
-        return 0;
+        return field.length;
+
     }
 
     void setFigure(final Point point, final Figure figure) {
+        field[point.x][point.y] = figure;
 
     }
 
     Figure getFigure(final Point point) {
 
-        return null;
+        return field[point.x][point.y];
+
     }
 }
