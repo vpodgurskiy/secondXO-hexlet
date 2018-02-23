be947f171acef13717f9da873f12359e53d7f529
v.podgurskiy <vpodgurskiy@gmail.com>
2018-02-23 06:25:16 +0300

    implement getWinner and empty test.

---
+++ tst/io/hexlet/xo/controller/WinnerControllerTest.java
@@ -0,0 +1,17 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Game;
+import io.hexlet.xo.model.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WinnerControllerTest {
+
+    @Test
+    public void getWinner() {
+
+
+    }
+}
