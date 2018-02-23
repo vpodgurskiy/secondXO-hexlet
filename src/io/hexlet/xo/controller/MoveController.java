75a5e92ac985984ed1ab80ed2752b569b8b50810
v.podgurskiy <vpodgurskiy@gmail.com>
2018-02-23 09:22:06 +0300

    applyFigure have been implementation.

--- src/io/hexlet/xo/controller/MoveController.java
+++ src/io/hexlet/xo/controller/MoveController.java
@@ -2,13 +2,17 @@ package io.hexlet.xo.controller;
 
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class MoveController {
 
-    void applyFigure(final Field field, final Figure figure, final Point point) {
+    void applyFigure(final Field field, final Figure figure, final Point point)
+                    throws AlreadyOccupiedException, InvalidPointException {
 
-        //field.[point.x][point.y] = figure;
+        field.setFigure(point, figure);
 
     }
 }
