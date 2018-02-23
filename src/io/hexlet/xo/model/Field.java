0d2df61f0e2c225e324bb7d7c44411f91a59eb72
v.podgurskiy <vpodgurskiy@gmail.com>
2018-02-23 08:44:35 +0300

    implement exceptions in methods

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,30 +1,56 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
 
+    private final int MIN_COORDINATE = 0;
+
+    private final int MAX_COORDINATE;
+
     private final Figure[][] field;
 
-    Field(final int FIELD_SIZE) {
+    public Field(final int FIELD_SIZE) {
+        this.MAX_COORDINATE = FIELD_SIZE;
         this.field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     }
 
-    int getSize() {
-
+    public int getSize() {
         return field.length;
-
     }
 
-    void setFigure(final Point point, final Figure figure) {
-        field[point.x][point.y] = figure;
+    public Figure getFigure(final Point point) throws InvalidPointException {
 
-    }
-
-    Figure getFigure(final Point point) {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
 
         return field[point.x][point.y];
-
     }
+
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
+                                                                         AlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
+        if (getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field[point.x][point.y] = figure;
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+
 }
