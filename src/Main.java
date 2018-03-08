da4726f627eb28d36b90fcf5e3adf9e269c0fa2a
v.podgurskiy <vpodgurskiy@gmail.com>
2018-03-08 20:55:43 +0300

    delete not use import class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Game;
+import io.hexlet.xo.view.ConsolView;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+
+        final ConsolView consolView = new ConsolView();
+
+        final Field field = new Field(3);
+
+        final Game game = new Game("XO", null, field);
+
+        consolView.show(game);
+
+        while (consolView.move(game)) {
+            consolView.show(game);
+        }
+
 
     }
 }
+
+
